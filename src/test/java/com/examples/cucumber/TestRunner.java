cef19a18984f00b17a71d2c87a849f50ffadd8e3
Ivaylo <ivaylo.goranovski@gmail.com>
2022-06-15 21:31:56 +0300

    commit 1

---
+++ src/test/java/com/examples/cucumber/TestRunner.java
@@ -0,0 +1,13 @@
+package com.examples.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features/testing.feature"},
+        glue="StepDefinitions")
+
+public class TestRunner {
+}
